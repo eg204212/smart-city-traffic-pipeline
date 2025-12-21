f1e132e2f3a3888c2103fb920d7ac8e441ed8d2e
eg4212 <chithramalisewwandi20@gmail.com>
2025-12-21 19:58:40 +0530

    updated docker containers

--- producer/traffic_producer.py
+++ producer/traffic_producer.py
@@ -3,7 +3,7 @@ from kafka import KafkaProducer
 from datetime import datetime
 
 producer = KafkaProducer(
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=['kafka:9092'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
 
